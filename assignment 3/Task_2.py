937954b75259c261d771359f9db328c96628ee22
Rohit-Mangla <rohitmangla018@gmail.com>
2026-01-25 11:22:52 +0530

    Add files via upload

---
+++ assignment 3/Task_2.py
@@ -0,0 +1,11 @@
+import math
+num = int(input("Enter a number: "))
+
+sqrt_value = math.sqrt(num)
+print("Square root: ",sqrt_value)
+
+log_value = math.log(num)
+print("Logarithm: ",log_value)
+
+sin_value = math.sin(num)
+print("Sine: ",sin_value)
